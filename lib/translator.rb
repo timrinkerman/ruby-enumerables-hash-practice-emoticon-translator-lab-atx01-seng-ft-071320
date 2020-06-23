c97754a0c720512a5d803f39059c55490b389306
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-23 15:39:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,7 +37,7 @@ def get_english_meaning(file_path, emoticon)
     #binding.pry
     if translations[:japanese] == emoticon
       return word
-    
+    end
     elsif !translations[:japanese] == emoticon
       p "Sorry, that emoticon was not found"
     end
