25ca8e1d1f795d6405dedda52ac4c035c372df04
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-23 15:10:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning(file_path, emoticon)
-  library = load_library("lib/emoticons.yml")
+  library = load_library(file_path)
   library.each do |key, value|
     #binding.pry
     if emoticon = value
