d8419963a6ce7401bdbbc15efed0f1f569e494b5
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-23 14:48:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,45 @@
 # require modules here
+require "yaml"
+require "pry"
+#create a key and assign the values to them
+#
 
-def load_library
-  # code goes here
+#finalHash = original hash << new_hash 
+
+#new_hash = :english => "value[0]",
+#           :japanese => "value[1]"
+                        
+def load_library(file_path)
+#access the array #create an empty hash for the new info to go to
+
+emoticons_hash = YAML.load_file("lib/emoticons.yml")
+final_hash = {
+
+ #access a key of an emotion 
+  new_key = emo
+
+
+
+other_key = emoticons_hash[:japanese]
+  
+  emoticons_hash.each_with_object({}) do |(emotion_word, faces), final_hash| 
+        new_key = faces[0]
+        other_key = faces[1]
+        
+   if !final_hash[emotion_word]
+     final_hash[emotion_word] = {}    
+    end
+    if final_hash[emotion_word]
+      final_hash[emotion_word] = []
+    end
+end 
+final_hash
 end
 
+
+
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
